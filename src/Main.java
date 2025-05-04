6f171e4c0a9b29862a91c59af658e3068e10dbd8
Marat Gaptulazyanov <gap.marat@gmail.com>
2025-05-04 12:07:38 +0400

    Update comments in Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 88.7;   // килограммы
-        double height = 1.75;   // метры
+        double weight = 88.7;   // Вес в килограммах
+        double height = 1.75;   // Рост в метрах
         double bmiIndex = service.calculate(weight,height);
         System.out.println("Ваш ИМТ составляет: " + bmiIndex);
     }
